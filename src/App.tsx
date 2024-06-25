9346950e9d9561b1d06c6bc12ad94ce421f82c09
MariuszRozycki <mariansc83@icloud.com>
2024-06-25 15:07:02 +0200

    Migrate countries-rest-api from another directory

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+import SearchCountriesContainer from './assets/containers/SearchCountriesContainer'
+
+function App() {
+
+  return (
+    <>
+      <SearchCountriesContainer />
+    </>
+  )
+}
+
+export default App
